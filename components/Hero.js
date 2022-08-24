d32f69a3d7f58eb54a585ae77b18171391eeb5b7
cooperbuilt <evanncooper@gmail.com>
2022-08-23 21:38:15 -0400

    wip: mocking out the hero

--- components/Hero.js
+++ components/Hero.js
@@ -1,5 +1,31 @@
+import RuntimeLogo from '../public/runtime-rundown-logo.png';
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from '../styles/Hero.module.css';
+
 const Hero = () => {
-  return <div>hey</div>;
+  return (
+    <div className={styles.Hero}>
+      <div>
+        <Link href="/">
+          <a>
+            <Image
+              className={styles.HeroLogo}
+              src={RuntimeLogo}
+              alt="Runtime Rundown Logo: Depicting a laptop with the words Runtime Rundown"
+              layout="responsive"
+            />
+          </a>
+        </Link>
+      </div>
+      <div>
+        <h2>Two Friends:</h2>
+        <div>hey</div>
+        <div>hey</div>
+        <h2>Talking About Web Development</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
