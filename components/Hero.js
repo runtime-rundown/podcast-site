98a03ad23a6e119b62f664d49487475e579a9273
cooperbuilt <evanncooper@gmail.com>
2022-08-24 19:39:35 -0400

    wip: mobile hero styles

--- components/Hero.js
+++ components/Hero.js
@@ -6,7 +6,7 @@ import styles from '../styles/Hero.module.css';
 const Hero = () => {
   return (
     <div className={styles.Hero}>
-      <div className={styles.HeroLogo}>
+      {/* <div className={styles.HeroLogo}>
         <Link href="/">
           <Image
             className={styles.HeroLogo}
@@ -15,41 +15,50 @@ const Hero = () => {
             layout="responsive"
           />
         </Link>
-      </div>
-      <h2>Breaking Down Interesting Web Dev Blog Posts And Videos</h2>
-      <Link href="/submit">
-        <a className={styles.HeroSubmit}>Suggest A Post / Video</a>
-      </Link>
+      </div> */}
+      <h1 className={styles.HeroTitle}>Runtime Rundown</h1>
+      <h2 className={styles.HeroTitleText}>
+        Breaking Down Interesting Web Dev Blog Posts And Videos
+      </h2>
+      <hr className={styles.HeroLine} data-content="The Hosts" />
       <div className={styles.HeroIntro}>
         <div className={styles.HeroFriends}>
-          <div className={styles.HeroFriendWrapper}>
-            <div className={styles.HeroFriend}>
-              <img
-                src="https://avatars.githubusercontent.com/cooperbuilt?size=200"
-                alt="Evan Cooper"
-                className={styles.HeroImage}
-              />
-              <div>
-                <h3>Evan Cooper</h3>
-                <a href="https://github.com/Cooperbuilt">@Cooperbuilt</a>
-                <p>Frontend Engineer at Amazon, former U.S Army Infantry</p>
+          <a href="https://github.com/Cooperbuilt" target="_blank">
+            <div className={styles.HeroFriendWrapper}>
+              <div className={styles.HeroFriend}>
+                <img
+                  src="https://avatars.githubusercontent.com/cooperbuilt?size=200"
+                  alt="Evan Cooper"
+                  className={styles.HeroImage}
+                />
+                <div className={styles.HeroInformation}>
+                  <h3>Evan Cooper</h3>
+                  <p>Frontend Engineer at Amazon, former U.S Army Infantry</p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className={styles.HeroFriendWrapper}>
-            <div className={styles.HeroFriend}>
-              <img
-                src="https://avatars.githubusercontent.com/helloitsjoe?size=200"
-                alt="Joe Boyle"
-                className={styles.HeroImage}
-              />
-              <div>
-                <h3>Evan Cooper</h3>
-                <a href="https://github.com/helloitsjoe">@HelloItsJoe</a>
-                <p>Frontend Engineer at Wayfair, Learning Japanese, Other stuff</p>
+          </a>
+          <a href="https://github.com/helloitsjoe" target="_blank">
+            <div className={styles.HeroFriendWrapper}>
+              <div className={styles.HeroFriend}>
+                <img
+                  src="https://avatars.githubusercontent.com/helloitsjoe?size=200"
+                  alt="Joe Boyle"
+                  className={styles.HeroImage}
+                />
+                <div>
+                  <h3>Joe Boyle</h3>
+                  <p>Frontend Engineer at Wayfair, Learning Japanese, Other stuff</p>
+                </div>
               </div>
             </div>
-          </div>
+          </a>
+        </div>
+        <hr className={styles.HeroLine} data-content="The Call to Action" />
+        <div className={styles.HeroSubmitWrapper}>
+          <Link href="/submit">
+            <a className={styles.HeroSubmit}>Suggest A Post / Video</a>
+          </Link>
         </div>
       </div>
     </div>
