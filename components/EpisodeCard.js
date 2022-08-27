90306ec55886b66e7a151074950ac70167b27821
cooperbuilt <evanncooper@gmail.com>
2022-08-27 10:31:47 -0400

    feat: add episode card with styles

--- components/EpisodeCard.js
+++ components/EpisodeCard.js
@@ -6,18 +6,53 @@ const processTitle = title => {
   return title.toLowerCase().replaceAll(' ', '-');
 };
 
-const EpisodeCard = ({ href, title, isoDate, ...rest }) => {
+function truncate(str, n, useWordBoundary = true) {
+  if (str.length <= n) {
+    return str;
+  }
+  const subString = str.slice(0, n - 1); // the original check
+  return (
+    (useWordBoundary
+      ? subString.slice(0, subString.lastIndexOf(' '))
+      : subString) + '...'
+  );
+}
+
+function getBackgroundColor() {
+  const int = Math.floor(Math.random() * 8);
+  return colors[int];
+}
+
+const colors = {
+  0: '--color-accent-pink',
+  1: '--color-accent-purple',
+  2: '--color-accent-teal',
+  3: '--color-base-teal',
+  4: '--color-accent-pink',
+  5: '--color-accent-purple',
+  6: '--color-accent-teal',
+  7: '--color-base-teal',
+};
+
+const EpisodeCard = ({ href, title, isoDate, contentSnippet, ...rest }) => {
   console.log(rest);
   return (
     <div
-      style={{ backgroundColor: 'var(--color-accent-pink)' }}
+      style={{ backgroundColor: `var(${getBackgroundColor()})` }}
       className={styles.episodeCardWrapper}
     >
       <Link href={`/episodes/${processTitle(title)}`}>
         <a className={styles.episodeCard}>
           <div className={styles.episodeCardInternals}>
-            <div className="font-bold">{title}</div>
-            <div>{format(new Date(isoDate), 'PPP')}</div>
+            <div className={styles.episodeDate}>
+              {format(new Date(isoDate), 'PP')}
+            </div>
+            <div>
+              <h3 className={styles.episodeTitle}>{title}</h3>
+              <div className={styles.episodeSnippet}>
+                {truncate(contentSnippet, 100)}
+              </div>
+            </div>
           </div>
         </a>
       </Link>
