7041c2210423ee2b9bb2402f4bac90852620a7d0
cooperbuilt <evanncooper@gmail.com>
2022-08-29 09:45:16 -0400

    wip: explicitly add hero to index

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import { FEED, getFeed } from '../feeds/rss';
+import Hero from '../components/Hero';
 import EpisodeCard from '../components/EpisodeCard';
 import styles from '../styles/Index.module.css';
 
 export default function Home({ feed, items }) {
   return (
     <>
+      <Hero />
       <h2>All Episodes</h2>
       <div className={styles.gridWrapper}>
         {items.map(item => {
