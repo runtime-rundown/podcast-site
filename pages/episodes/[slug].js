95b094c16288df1860c8892abe0601a6a66429a2
cooperbuilt <evanncooper@gmail.com>
2022-08-29 09:45:03 -0400

    wip: episode page work, adding utterances

--- pages/episodes/[slug].js
+++ pages/episodes/[slug].js
@@ -1,18 +1,47 @@
+import styles from '../../styles/EpisodePage.module.css';
+import Hero from '../../components/Hero';
 import { FEED, getFeed } from '../../feeds/rss';
+import Script from 'next/script';
 
-const processTitle = (title) => {
+const processTitle = title => {
   return title.toLowerCase().replaceAll(' ', '-');
 };
 
-const EpisodePage = ({ pubDate, content, enclosure: { url: src } }) => {
+const EpisodePage = ({
+  pubDate,
+  content,
+  enclosure: { url: src },
+  ...episodeDetails
+}) => {
+  console.log(episodeDetails);
   return (
     <>
-      <div>{pubDate}</div>
-      <audio controls>
-        <source src={src} type="audio/mpeg" />
-        Your browser does not support the audio element.
-      </audio>
-      <div dangerouslySetInnerHTML={{ __html: content }} />
+      <Hero isShort />
+      <div className={styles.episodePage}>
+        <h3>{episodeDetails.title}</h3>
+        <audio controls>
+          <source src={src} type="audio/mpeg" />
+          Your browser does not support the audio element.
+        </audio>
+        <div
+          className={styles.episodeDetails}
+          dangerouslySetInnerHTML={{
+            __html: content.replace(
+              /href/g,
+              "target='_blank' rel='noreferrer' href",
+            ),
+          }}
+        />
+      </div>
+      <Script
+        src="https://utteranc.es/client.js"
+        repo="Cooperbuilt/runtime-rundown"
+        issue-term="title"
+        label="suggestion"
+        theme="github-light"
+        crossorigin="anonymous"
+        async
+      />
     </>
   );
 };
@@ -22,7 +51,7 @@ export default EpisodePage;
 // produces a page for every episode
 export async function getStaticPaths() {
   const detailedFeed = await getFeed(FEED.url);
-  const paths = detailedFeed.items.map((feedItem) => ({
+  const paths = detailedFeed.items.map(feedItem => ({
     params: { slug: `${processTitle(feedItem.title)}` },
   }));
 
@@ -38,7 +67,7 @@ export async function getStaticProps(context) {
     params: { slug },
   } = context;
   const { items } = await getFeed(FEED.url);
-  const episode = items.find((item) => {
+  const episode = items.find(item => {
     return processTitle(item.title) === slug;
   });
   return {
