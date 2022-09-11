e0f053c86f57fd9fe552aec1d93279504f084ea1
cooperbuilt <evanncooper@gmail.com>
2022-09-10 21:39:15 -0400

    wip: suggestions page work

--- pages/suggestions.js
+++ pages/suggestions.js
@@ -5,10 +5,14 @@ import LogoBanner from '../components/LogoBanner';
 
 const Suggestions = () => {
   return (
-    <>
+    <div className={styles.suggestions}>
       <LogoBanner />
-      <div className={styles.suggestions}>hey</div>
-    </>
+      <h2>Get Involved in the Show</h2>
+      <p>
+        We have a few ways to get involved. First up is the message board below.
+        You can use this to give us a suggestion, shout out the show, etc.{' '}
+      </p>
+    </div>
   );
 };
 
