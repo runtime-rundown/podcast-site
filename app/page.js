4ba3fd0b4affd909199b894d17a374f3f19265ab
helloitsjoe <jboyle617@gmail.com>
2023-07-07 23:59:47 -0400

    consolidate

--- app/page.js
+++ app/page.js
@@ -1,10 +1,22 @@
-import Home from './home-page';
+import Hero from '../components/Hero';
+import EpisodeCard from '../components/EpisodeCard';
+import styles from '../styles/Index.module.css';
 import { FEED, getFeed } from '../feeds/rss';
 
 export const revalidate = 60;
 
 export default async function Page() {
-  const detailedFeed = await getFeed(FEED.url);
+  const { items } = await getFeed(FEED.url);
 
-  return <Home feed={FEED} items={detailedFeed.items} />;
+  return (
+    <>
+      <Hero />
+      <h2>All Episodes</h2>
+      <div className={styles.gridWrapper}>
+        {items.map(item => {
+          return <EpisodeCard {...item} key={item.title} />;
+        })}
+      </div>
+    </>
+  );
 }
